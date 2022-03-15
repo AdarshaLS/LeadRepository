4e0cde760286dbc46c295d24ee976c19c5591331
Lenovo <Lenovo@DESKTOP-01H70DH>
2022-03-15 07:55:02 +0530

    Jayanth first commit

---
+++ src/test/java/ProductTest/ProductTest.java
@@ -0,0 +1,14 @@
+package ProductTest;
+
+import org.testng.annotations.Test;
+
+public class ProductTest {
+
+	@Test
+	public void createProductTest()
+	{
+		System.out.println("Product page is created");
+	}
+	
+	
+}
